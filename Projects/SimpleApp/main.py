f7241a9f913606b6a138764eda573a33c2be2472
Naruto08403 <arguellesruel03@gmail.com>
2024-10-22 16:55:55 +0800

    Add files via upload

--- Projects/SimpleApp/main.py
+++ Projects/SimpleApp/main.py
@@ -1,11 +1,28 @@
-from kivymd.app import MDApp 
-from kivy.lang import Builder 
+from kivymd.app import MDApp
+from kivy.lang import Builder
 
 
-class SimpleApp(MDApp):
-    def build(self):
-        return Builder.load_file('design.kv')
+class ProjectApp(MDApp):
+
+    def on_start(self):
+        self.loginscreen = Builder.load_file('login.kv')
+        self.homescreen = Builder.load_file('home.kv')
+        self.signupscreen = Builder.load_file('signup.kv')
+
+
+        self.root.add_widget(self.loginscreen)
+        self.root.add_widget(self.homescreen)
+        self.root.add_widget(self.signupscreen)
+        
+
+    def login(self):
+        username = self.loginscreen.ids.username.text 
+        password = self.loginscreen.ids.password.text 
+        if username == 'admin' and password == 'admin':
+            self.root.current = 'home'
+        else:
+            self.loginscreen.ids.errorlabel.text = 'Wrong credentials!'
 
 
 if __name__ == '__main__':
-    SimpleApp().run()
+    ProjectApp().run()
